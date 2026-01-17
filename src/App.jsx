03207af06059c82405d1e7451be0874b2e2bb6ef
evandereth <ethevander@gmail.com>
2026-01-17 14:28:14 +0100

    first upload for ongoing portfolio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Profile from "./components/Profile"
+
+
+function App() {
+
+  return (
+   <>
+    <div className="w-[90%] mx-auto bg-green-200">
+      <section>
+        <Profile />
+      </section>
+    </div>
+   </>
+  )
+}
+
+export default App
